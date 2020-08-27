6c483d85af172bd400cb5cbeb616bf9d3303cb3e
Jeremy Wood <js.wood206@gmail.com>
2020-08-27 03:28:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,13 @@ require 'pry'
 
 def prime(int)
   number_list = (2..int).to_a
-  binding.pry
+  number_list.map do |x| 
+    if x % int == 0 
+      true
+      break
+    else
+      false
+    end
+  end
 
 end
