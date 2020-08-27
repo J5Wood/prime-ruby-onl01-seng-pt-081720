04f2b47a376e2ce5953a8828bb3a768374b41628
Jeremy Wood <js.wood206@gmail.com>
2020-08-27 03:30:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,7 @@ require 'pry'
 
 def prime(int)
   number_list = (2..int).to_a
-  number_list.map do |x|
-    if x % int == 0
-      true
-      break
-    else
-      false
-    end
-  end
+  number_list.any? { |x| int % x == 0}
+    
 
 end
